673242f338cf2ef226d6a676506cac4f8983e83f
Christopher Dismuke <cdismuke@christophersmbp.lan>
2021-07-24 20:22:28 -0500

    first commit

---
+++ id-voyages.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+tracking = pd.read_excel('with-distance.xlsx')
+print(tracking.describe())
---
+++ start.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from scipy.spatial.distance import cdist
+from geopy.distance import geodesic
+
+def closest_point(point, points):
+    return points[cdist([point], points).argmin()]
+
+tracking  = pd.read_csv('tracking.csv')
+ports = pd.read_csv('ports.csv')
+
+print(tracking.tail())
+print(ports.tail())
+
+points = list(ports[['lat','long']].to_records(index=False))
+points2 = ports[['lat','long']].to_numpy().reshape(-1,2)
+print(points2)
+#print(points)
+'''
+for index, row in tracking.iterrows():
+    point = (row.lat, row.long)
+    print(point)
+    closest = closest_point( point, points2)
+    print(closest)
+    distance = geodesic(point, closest).mi
+    row['distance']=distance
+'''
+
+def return_distance(latitude, longitude, points):
+    point = (latitude,longitude)
+    closest = closest_point(point, points)
+    return geodesic(point, closest).mi
+
+tracking['distance'] = tracking.apply(lambda x: return_distance(x['lat'], x['long'], points2), axis=1)
+
+tracking.to_excel('with-distance.xlsx')
